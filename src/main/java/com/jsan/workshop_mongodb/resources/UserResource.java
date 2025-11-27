a3c1e933804d9a84392099d6f421d4f3d800ef95
JSampx <joao.sampaio.andrade@gmail.com>
2025-11-27 15:30:42 -0300

    feat: insert user in UserService

--- src/main/java/com/jsan/workshop_mongodb/resources/UserResource.java
+++ src/main/java/com/jsan/workshop_mongodb/resources/UserResource.java
@@ -4,13 +4,12 @@ import com.jsan.workshop_mongodb.domain.User;
 import com.jsan.workshop_mongodb.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.jsan.workshop_mongodb.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,4 +34,13 @@ public class UserResource {
 
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/com/jsan/workshop_mongodb/services/UserService.java
+++ src/main/java/com/jsan/workshop_mongodb/services/UserService.java
@@ -1,11 +1,14 @@
 package com.jsan.workshop_mongodb.services;
 
 import com.jsan.workshop_mongodb.domain.User;
+import com.jsan.workshop_mongodb.dto.UserDTO;
 import com.jsan.workshop_mongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.jsan.workshop_mongodb.repository.UserRepository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -22,4 +25,12 @@ public class UserService {
         User user = repo.findById(id).orElseThrow(()->new ObjectNotFoundException(id));
         return user;
     }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+  }
 }
