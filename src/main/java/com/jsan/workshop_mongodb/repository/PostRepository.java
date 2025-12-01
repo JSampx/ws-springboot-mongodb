dd3e54005aacf3e9d617c22cd2a277eda0d5125d
JSampx <joao.sampaio.andrade@gmail.com>
2025-12-01 16:44:05 -0300

    feat: Post domain created

---
+++ src/main/java/com/jsan/workshop_mongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jsan.workshop_mongodb.repository;
+
+import com.jsan.workshop_mongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
